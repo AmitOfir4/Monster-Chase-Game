972398c0cce864475f76fe2cda6f86f357525512
amito <amit.ofir4@gmail.com>
2024-11-01 21:12:54 +0200

    update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,7 +21,9 @@ public class Player : MonoBehaviour
     private string WALK_ANIMATION = "Walk";
     private string GROUND_TAG = "Ground";
     private bool isGrounded = true;
+    private bool hasShield = false;
     private string ENEMY_TAG = "Enemy";
+    private string COIN_TAG = "Coin";
 
     [SerializeField]
     private Timer timer;
@@ -58,7 +60,6 @@ public class Player : MonoBehaviour
 
     void AnimatePlayer()
     {
-        Debug.Log(gameObject.name);
         if (gameObject.name == "Rino(Clone)")
         {
             AnimateRino();
@@ -69,7 +70,6 @@ public class Player : MonoBehaviour
             {
                 anim.SetBool(WALK_ANIMATION, true);
                 sr.flipX = false;
-                Debug.Log(gameObject.name);
             }
             else if (movementX < 0)
             {
@@ -89,7 +89,6 @@ public class Player : MonoBehaviour
         {
             anim.SetBool(WALK_ANIMATION, true);
             sr.flipX = true;
-            Debug.Log(gameObject.name);
         }
         else if (movementX < 0)
         {
@@ -118,17 +117,37 @@ public class Player : MonoBehaviour
             isGrounded = true;
         }
 
-        if (collision.gameObject.CompareTag(ENEMY_TAG))
+        if (collision.gameObject.CompareTag(ENEMY_TAG) && !hasShield)
         {
             Destroy(gameObject);
+        }
+        
+        if (collision.gameObject.CompareTag(COIN_TAG))
+        {
+            Destroy(collision.gameObject);
+            GivePlayerShield(gameObject);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag(ENEMY_TAG))
+        if (collision.gameObject.CompareTag(ENEMY_TAG) && !hasShield)
         {
             Destroy(gameObject);
         }
+    }
+
+    private void GivePlayerShield(GameObject player)
+    {
+        hasShield = true;
+        Debug.Log("Shield activated for 10 seconds");
+        StartCoroutine(DisableShieldAfterTime(10f));
+    }
+    
+    private IEnumerator DisableShieldAfterTime(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        hasShield = false;
+        Debug.Log("Shield disabled after 10 seconds");
     }
 }
